9953af2da7bccd63b7630d7be421af0e03fb70ea
Keri Deemer <kerid18@att.net>
2019-09-05 23:47:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length 
+    new_array << yield(source_array[i])
+    i=+1 
+  end
+  new_array
+  
+end
