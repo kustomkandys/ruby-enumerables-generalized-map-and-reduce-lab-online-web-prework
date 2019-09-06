91b6303fb610b55f757b0ea62b3df5c1f9411bfd
Keri Deemer <kerid18@att.net>
2019-09-06 00:02:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def my_own_map(source_array)
   new_source_array = []
   counter = 0 
   while counter < source_array.length 
-    new_source_array.push(yield(source_array))
+    new_source_array.push(yield(source_array[counter]))
     
  
  end 
